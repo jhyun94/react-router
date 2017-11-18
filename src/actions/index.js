c519d4015346880703195f7de1f7f2527ba78337
jhyun94 <hyunjonathan@gmail.com>
2017-11-18 16:48:06 -0600

    post reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 const API_KEY = '/?key=JONJON123';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+
+
 export function fetchPOSTS(){
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
