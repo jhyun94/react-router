485ebe57c2e29968dacae9dba8456c26de869bf9
jhyun94 <hyunjonathan@gmail.com>
2017-11-18 15:56:46 -0600

    boilerplate for index route

--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route} from 'react-router-dom';
-
-import App from './components/app';
 import reducers from './reducers';
+
+import PostsIndex from './components/PostIndex';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component{ render(){ return <div>Hello</div>} };
-class Goodbye extends React.Component{ render(){ return <div>Goodbye</div>} };
+
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} />
-        <Route path="/bye" component={Goodbye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
