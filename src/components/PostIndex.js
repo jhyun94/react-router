31ec9705007930f875cc82bc881543fe0a78823e
jhyun94 <hyunjonathan@gmail.com>
2017-11-19 13:40:16 -0600

    able to render posts

--- src/components/PostIndex.js
+++ src/components/PostIndex.js
@@ -2,14 +2,27 @@ import React, { Component} from 'react';
 import { connect} from 'react-redux';
 import { FetchPosts} from '../actions';
 
+import _ from 'lodash';
+
 class PostsIndex extends Component{
   componentDidMount(){
     this.props.FetchPosts();
   }
 
+  renderPosts(){
+    return _.map(this.props.posts, (post) => {
+      return(
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
+
   render(){
     return(
-      <div>PostIndex</div>
+      <ul className="list-group">{this.renderPosts()}</ul>
     )
   }
 }
