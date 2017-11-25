f8a016fbc2782aaee390d2bba80617c24b2766b8
jhyun94 <hyunjonathan@gmail.com>
2017-11-25 13:41:49 -0600

    cancel button

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link} from 'react-router-dom';
 
 class PostNew extends Component{
 
@@ -39,6 +40,7 @@ class PostNew extends Component{
           name="content"
           component={ this.renderField} />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to='/' className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
