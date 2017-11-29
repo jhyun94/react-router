71c24e9d88df9b83a9e127275f29ca7292d4fe8b
jhyun94 <hyunjonathan@gmail.com>
2017-11-29 17:24:20 -0600

    action creator and reducer for fetch post

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POSTS = 'create_posts';
+export const FETCH_POST = 'fetch_post';
 const API_KEY = '/?key=JONJON123';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -20,6 +21,14 @@ export function createPost(values, callback){
       .then( () => callback() );
   return {
     type: CREATE_POSTS,
+    payload: request
+  }
+}
+
+export function FetchPost(id){
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: FETCH_POST,
     payload: request
   }
 }
