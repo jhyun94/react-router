4269d5bf1f5b12e4f55737565e294418d7dcd276
jhyun94 <hyunjonathan@gmail.com>
2017-12-01 17:15:06 -0600

    connect fetchpost action creator and get post

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FetchPost } from '../actions';
 
 class PostShow extends Component{
+  componentDidMount(){
+    const {id} = this.props.match.params;
+    this.props.FetchPost(id);
+  }
   render(){
     return (
       <div>Post Show!</div>
@@ -8,4 +14,8 @@ class PostShow extends Component{
   }
 }
 
-export default PostShow;
+function mapStateToProps({posts}, ownProps){
+  return { post: posts[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { FetchPost })(PostShow);
