8f211a2ce87bb2da3b83a9a458815fa2e36ca271
jhyun94 <hyunjonathan@gmail.com>
2017-12-02 13:39:32 -0600

    able to render home page component

---
+++ src/components/post_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostAll extends Component{
+  render(){
+    return(
+      <div>PostAll</div>
+    )
+  }
+}
+
+export default PostAll;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import reducers from './reducers';
+import PostAll from './components/post_index';
+
 
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -11,5 +15,10 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route to='/' component={PostAll} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
