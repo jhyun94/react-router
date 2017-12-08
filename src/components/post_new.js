5a6c0a7813e3f1c9eb589a12bfe64f2457a37b46
jhyun94 <hyunjonathan@gmail.com>
2017-12-08 15:54:39 -0600

    able to display form using redux form

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,11 +1,39 @@
 import React, { Component} from 'react';
+import { Field, reduxForm} from 'redux-form';
 
 class PostNew extends Component{
+
+  renderInput(field){
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input type="text" className="form-control" />
+      </div>
+    )
+
+  }
   render(){
     return(
-      <div>Post New!</div>
+      <div>
+        <form>
+          <Field
+          label="Post Title"
+          name="title"
+          component={this.renderInput}/>
+          <Field
+          label="Post Categories"
+          name="categories"
+          component={this.renderInput}/>
+          <Field
+          label="Post Content"
+          name="content"
+          component={this.renderInput}/>
+        </form>
+      </div>
     )
   }
 }
 
-export default PostNew;
+export default reduxForm({
+  form: 'post'
+})(PostNew);
