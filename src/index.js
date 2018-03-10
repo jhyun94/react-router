b2da2a7ec1b2a9e82f2e1c0821722f90a8668e24
jhyun94 <hyunjonathan@gmail.com>
2018-03-09 20:48:07 -0600

    initial

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import reduxPromise from 'redux-promise';
 
-import reducers from './reducers';
-import PostAll from './components/post_index';
-import PostNew from './components/post_new';
-import PostShow from './components/post_show';
+
 
 
 
@@ -18,14 +13,5 @@ const createStoreWithMiddleware = applyMiddleware(reduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path='/post/new' component={PostNew} />
-          <Route path='/post/:id' component={PostShow} />
-          <Route path='/' component={PostAll} />
-        </Switch>
-      </div>
-    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,7 @@
 import { combineReducers } from 'redux';
-import Posts from './reducer_posts'; 
-import { reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
-  posts: Posts,
-  form: formReducer
+
 });
 
 export default rootReducer;
